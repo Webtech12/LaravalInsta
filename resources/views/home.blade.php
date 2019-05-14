5dfb0d7aa66d53395dc50a0ce172adea21076424
Webtech12 <sherwinlukes@gmail.com>
2019-05-14 14:41:08 +0500

    migrations commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,43 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="row">
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+  <div class="col-sm-3 p-3">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV8bzQ0NCsJHKF01mLwsTSrEh_i2noZhjAFl_VbkWWI-o4nYCP" 
+    class="rounded-circle">
+  </div>
+
+  <div class="col-9 p-5 pl-4 float-right">
+    <div><h1>TEST POST</h1></div>
+    <div class="d-flex">
+        <div class="pr-3"><strong>123</strong> posts</div>
+        <div class="pr-3"><strong>23k</strong> followers</div>
+        <div class="pr-3"><strong>211</strong> following</div>
     </div>
+
+    <div class="pt-4 font-weight-bold">dummy</div>
+
+  <div>A wiki is a knowledge base website on which users collaboratively modify content and structure directly from the web browser. In a typical wiki, text is written ...</div>
+  <div><a href="#">https://en.wikipedia.org/wiki/Wiki</a></div>
+  </div>
+  
+</div>
+
+<div class="row pt-5">
+
+    <div class="col-4">
+        <img src="https://educationaltechnology.net/wp-content/uploads/2017/09/wiki.gif" class="w-100">
+    </div>
+    <div class="col-4">
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png" class="w-100">
+    </div>
+    <div class="col-4">
+        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png" class="w-100">
+    </div>
+
+</div>
+
 </div>
 @endsection
